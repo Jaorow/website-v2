2ad57559dd3093d8669c5223e53299673ef96365
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-07-18 13:14:12 +1200

    blink still not working but getting close

--- logo_ch.js
+++ logo_ch.js
@@ -1,10 +1,14 @@
 function changeImage() {
-    var image = document.getElementById('myImage');
+    console.log("changing");
+    
+    var image = document.getElementsByClassName('blink');
+    console.log(image)
     if (image.src.match("img/logo.png")) {
         image.src = "img/logo_blink.png";
-    }
-    else {
+        console.log("close");
+    }else {
         image.src = "img/logo.png";
+        console.log("open");
     }
 }
 
@@ -14,10 +18,9 @@ function init() {
         changeImage();
         var rand = Math.round(Math.random() * (3000 - 500)) + 500; // generate new time (between 3sec and 500"s)
         setTimeout(myFunction, rand);
+        
     }
     myFunction();
 }
 
-$(function() {
-    init();
-});
+init();
