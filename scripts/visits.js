c64f0d2dd95029b98169291a859c0d212b24ae4b
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-11-17 17:49:32 +1300

    updage!

--- scripts/visits.js
+++ scripts/visits.js
@@ -3,6 +3,6 @@ function websiteVisits(response) {
 }
 
 function setVisits(number){
-    fetch('https://api.countapi.xyz/set/jaorow.com/89b89002-43a4-460a-ac65-858d507e00eb?value='+number)
+    fetch('https://api.countapi.xyz/set/jaorow.com/89b89002-43a4-460a-ac65-858d507e00eb?value='+(number-1))
     console.log('visits refreshed to ' + number +' please refresh page')
 }
